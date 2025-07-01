d162e82dc55cdaaee5e94a3f01570652b2454ece
saihari1 <167093570+saihari1@users.noreply.github.com>
2025-07-01 10:56:21 +0530

    Update index.js

--- polling-app-client/src/constants/index.js
+++ polling-app-client/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://3.233.76.169:8090/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://api.harinadh.xyz/api';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const POLL_LIST_SIZE = 30;
