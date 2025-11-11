b15282488cf75bf6e03cbba69c6118893f24d790
saihari1 <167093570+saihari1@users.noreply.github.com>
2025-11-12 00:25:47 +0530

    Update build.sh

--- polling-app-server/build.sh
+++ polling-app-server/build.sh
@@ -7,15 +7,13 @@ git clone https://github.com/saihari1/polls-app-new.git
 echo "=== Moving into backend project ==="
 cd polls-app-new/polling-app-server
 
-echo "=== Installing Maven ==="
-apt-get update -y
-apt-get install -y maven
+
 
 echo "=== Building the project ==="
 # If you have a bash build script:
 # chmod +x build.sh && ./build.sh
 
 # Or just run Maven directly:
-mvn clean install -DskipTests
+mvn clean install 
 
 echo "=== Build completed successfully ==="
