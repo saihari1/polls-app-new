64d9856a6c1b55d5e7a3a0c4115720cc44306e50
saihari1 <167093570+saihari1@users.noreply.github.com>
2026-04-18 04:46:36 +0530

    Update index.js

--- polling-app-client/src/constants/index.js
+++ polling-app-client/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://merry-adventure.railway.internal:3000/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://reliable-charisma-production.up.railway.app/api';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const POLL_LIST_SIZE = 30;
