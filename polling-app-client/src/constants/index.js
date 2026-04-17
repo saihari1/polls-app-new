abe2eb70d0869464e96c086b65559733b2a49d43
saihari1 <167093570+saihari1@users.noreply.github.com>
2026-04-18 01:18:23 +0530

    Update index.js

--- polling-app-client/src/constants/index.js
+++ polling-app-client/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://roundhouse.proxy.rlwy.net:16001/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://nozomi.proxy.rlwy.net:51774/api';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const POLL_LIST_SIZE = 30;
